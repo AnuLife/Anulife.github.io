c6dfa0e24ab3b5c9504172fe7aeab9e66c0c96cf
TILucario <tilucario@TILucariodeMBP.lan>
2021-01-12 10:50:23 +0800

    Site updated: 2021-01-12 10:50:22

--- js/custom.js
+++ js/custom.js
@@ -122,12 +122,11 @@ $(document).ready(function () {
 
 //回到旧站
 $(".backtoanulife").mouseenter(function () {
-    $(this).css({ boxShadow: "5px 5px 0px #FD4300" });
+    $(this).css({ boxShadow: "0" });
     $(this).css({ backgroundColor: "#141145" });
 });
-
 $(".backtoanulife").mouseleave(function () {
-    $(this).css({ boxShadow: "5px 5px 0px #141145" });
+    $(this).css({ boxShadow: "rgb(20, 17, 69) 2px 2px 0px;" });
     $(this).css({ backgroundColor: "#FD4300" });
 });
 
