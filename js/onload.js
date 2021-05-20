f4237940ed70b7d8532b0cf76f15364113357d26
TILucario <tilucario@TILucariodeMBP.lan>
2021-05-20 15:21:44 +0800

    Site updated: 2021-05-20 15:21:43

--- js/onload.js
+++ js/onload.js
@@ -3,7 +3,19 @@ var u = navigator.userAgent;
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
 var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 var windowWidth = $(window).width();
+
+//获取当前时间
+var ifDrink = (new Date()).getHours()
+console.log(ifDrink)
+
 $(function () {
+
+    //如果当前时间是三点或凌晨三点则播放视频
+    if(ifDrink == 15 || ifDrink == 3){
+        $(".drink-time").show()
+    }
+
+    
     //判断访问设备
     if(isAndroid == true || isiOS == true) {
         // alert('安卓端')
